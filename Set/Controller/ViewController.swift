f972a582eedfea22267a8c2df668156d9d486666
Sam <sam.germain@usask.ca>
2019-11-20 14:00:39 -0600

    Top Left corner is in middle of screen

--- Set/Controller/ViewController.swift
+++ Set/Controller/ViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class ViewController: UIViewController, CardViewDelegate {
 
     @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet var cardViews: [CardView]!
+    @IBOutlet weak var cardViewHolder: UIView!
+    @IBOutlet weak var cardViewHolderInnerView: UIView!
+    
+    var cardViews = [CardView]()
     @objc var game = SetGame()
     private var scoreObs: NSKeyValueObservation?    //KVO for the score
     
@@ -25,6 +28,7 @@ class ViewController: UIViewController, CardViewDelegate {
         for card in self.cardViews{
             card.delegate = self
         }
+        
     }
 
     /**
@@ -36,16 +40,81 @@ class ViewController: UIViewController, CardViewDelegate {
         }
     }
     
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+
+        coordinator.animate(alongsideTransition: { _ in
+        }) { [unowned self] _ in
+            self.updateViewFromModel()
+        }
+
+    }
+    
     private func updateViewFromModel(){
         for index in game.board.cards.indices{
+            let frame = CGRect(x: 50, y: 50, width: 50, height: 80)
+            if index > cardViews.count - 1{
+                cardViews.append(CardView(frame: frame))
+            }
             let cView = cardViews[index]
             let card = game.board.cards[index]
 
             cView.setAttributes(card: card)
             cView.layer.borderColor = UIColor(named: card.state.rawValue)?.cgColor
-            cView.isHidden = false
+            if !cardViewHolderInnerView.subviews.contains(cView){
+                cardViewHolderInnerView.addSubview(cView)
+            }
+            
+            let (w,h) = cardWidthHeight()
+            var x:CGFloat = 0.0
+            var y:CGFloat = 0.0
+            print(w)
+            print(h)
+            print(cardViewHolder.frame.size.width)
+            print(cardViewHolder.frame.size.height)
+            
+            cardViewHolderInnerView.subviews.forEach { v in
+
+                v.frame = CGRect(x: x, y: y, width: w, height: h)
+                x += w
+                if x + w > cardViewHolder.frame.size.width {
+                    x = 0.0
+                    y += h
+                }
+
+            }
+            
+            //cView.isHidden = false
             //Way to tell if you've made a match, mismatch or selection
         }
+    }
+    
+    func cardWidthHeight() -> (CGFloat, CGFloat){
+        let containerWidth = cardViewHolder.frame.size.width
+        let containerHeight = cardViewHolder.frame.size.height
+        let numItems = CGFloat(cardViewHolderInnerView.subviews.count)
+        var numCols = numItems
+        var numRows: CGFloat = 1
+        
+        var w = containerWidth / numCols
+        var h = w * 8.0 / 5.0
+        
+        if h > containerHeight {
+            h = containerHeight
+            w = h * 5.0 / 8.0
+        }
+        
+         while h * numRows < containerHeight, numCols > 1 {
+             numCols -= 1
+             numRows = ceil(numItems / numCols)
+             h = containerHeight / numCols
+             w = h * 5.0 / 8.0
+         }
+        
+        return (w,h)
+        
+        
     }
     
     /**
@@ -59,8 +128,7 @@ class ViewController: UIViewController, CardViewDelegate {
     }
     
     func selectCard(from view: CardView) {
-        print("test2")
-        if let cardNumber = cardViews.firstIndex(of: view){
+       if let cardNumber = cardViews.firstIndex(of: view){
             game.selectCard(at: cardNumber)
             self.updateViewFromModel()
         }
